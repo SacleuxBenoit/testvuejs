fbaf57154ec08e3f5d561002d3489c88d06df088
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-23 09:31:07 +0100

    add {{title}} in src/App.vue

--- testvuejs/src/App.vue
+++ testvuejs/src/App.vue
@@ -9,7 +9,7 @@
     <v-toolbar dense>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-title> {{ title }}</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
