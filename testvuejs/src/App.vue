a7fb36e1a917a52ffc2a764f8d1dcd16e2647c87
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-23 09:27:28 +0100

    delete import/export for HelloWorld.vue in src/App.vue

--- testvuejs/src/App.vue
+++ testvuejs/src/App.vue
@@ -30,14 +30,13 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
 
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld,
+    
   },
 
   data: () => ({
