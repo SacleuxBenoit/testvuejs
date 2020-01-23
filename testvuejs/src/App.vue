f2bbb8b32fc3d47cb091e4c9f74252c7abeaaa2e
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-23 09:39:55 +0100

    change the template in src/App.vue

--- testvuejs/src/App.vue
+++ testvuejs/src/App.vue
@@ -1,31 +1,12 @@
 <template>
 <v-app>
-  <v-card
-    color="grey lighten-4"
-    flat
-    height="200px"
-    tile
-  >
-    <v-toolbar dense>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title> {{ title }}</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-toolbar>
-  </v-card>
+<v-toolbar>
+  <v-toolbar-title>{{title}}</v-toolbar-title>
+  <v-spacer></v-spacer>
+  <v-toolbar-items class="hidden-sm-and-down">
+    <v-btn flat v-bind:keys='menu in menus'>{{menu.name}}</v-btn>
+  </v-toolbar-items>
+</v-toolbar>
 </v-app>
 </template>
 
