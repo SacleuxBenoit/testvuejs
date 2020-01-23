bf19cb828701b7efa013697ba776e5e8d44b2c65
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-23 16:06:38 +0100

    add import/export for HelloWorld

--- testvuejs/src/views/About.vue
+++ testvuejs/src/views/About.vue
@@ -1,9 +1,12 @@
 <template>
-  
+<HelloWorld />
 </template>
 
 <script>
+import HelloWorld from "@/components/HelloWorld.vue";
 export default {
-  
+  component: {
+    HelloWorld,
+  }
 }
 </script>
