b4f852feae19d339d74b1417f261c7f01f20d055
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-28 21:10:44 +0100

    add the import/export of the component HelloWorld in Friends.vue

--- testvuejs/src/views/Friends.vue
+++ testvuejs/src/views/Friends.vue
@@ -1,9 +1,18 @@
 <template>
-    
+  <div class="home">
+    <HelloWorld />
+    <Footer />
+  </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
 export default {
-    
-}
+  name: "home",
+  components: {
+    HelloWorld,
+  }
+};
 </script>
