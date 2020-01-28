9216559f0197b7297293827e1608cbbd92a77782
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-28 21:13:51 +0100

    add the import/export of the component Header in Friends.vue

--- testvuejs/src/views/Friends.vue
+++ testvuejs/src/views/Friends.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="home">
     <Footer />
+    <Header/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Footer from "@/components/Footer.vue";
-
+import Header from "@/components/Header.vue";
 
 
 export default {
   name: "home",
   components: {
-    Footer
+    Footer,
+    Header
   }
 };
 </script>
