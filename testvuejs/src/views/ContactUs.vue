8b824ec6f4d2417c9c8178a33b480407dc9212b6
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-28 21:15:56 +0100

    add the import/export for the component Footer in ContactUs.vue

--- testvuejs/src/views/ContactUs.vue
+++ testvuejs/src/views/ContactUs.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <Header/>
+    <Header />
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from "@/components/Header.vue";
-
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "home",
   components: {
-    Header
+    Header,
+    Footer
   }
 };
 </script>
