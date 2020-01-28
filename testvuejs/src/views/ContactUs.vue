7436c83287632e6bcf89f30af2697be7f78e7eec
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-28 21:15:03 +0100

    add the import/export for the component header in ContactUs.vue

--- testvuejs/src/views/ContactUs.vue
+++ testvuejs/src/views/ContactUs.vue
@@ -1,9 +1,18 @@
 <template>
-    
+  <div class="home">
+    <Header/>
+  </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+
+
 export default {
-    
-}
+  name: "home",
+  components: {
+    Header
+  }
+};
 </script>
