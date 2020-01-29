03596da2aeb4fefe989bc0f25ee219e7b4173810
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-29 18:51:28 +0100

    add the import/export for the component Footer in views/About.vue

--- testvuejs/src/views/About.vue
+++ testvuejs/src/views/About.vue
@@ -2,17 +2,21 @@
 <div>
   <Header />
 <HelloWorld />
+<Footer />
 </div>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue";
 import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
 
 export default {
   component: {
     HelloWorld,
-    Header
+    Header,
+    Footer
   }
 }
 </script>
