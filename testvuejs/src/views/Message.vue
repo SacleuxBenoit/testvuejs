21e3545b3a0c7cbc5822d46616a05b73a9a2a90f
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-29 18:43:10 +0100

    delete the import/export for the component HelloWorld in views/Message.vue

--- testvuejs/src/views/Message.vue
+++ testvuejs/src/views/Message.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <header />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import Header from "@/components/Header.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld,
     Header
   }
 };
