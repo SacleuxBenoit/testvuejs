2fbf366d2e4f4b32cb67da66fd1fcbfe5fbc2a43
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-29 19:08:18 +0100

    rename the file views/Message.vue to views/MessagePrivate.vue

---
+++ testvuejs/src/views/MessagePrivate.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <Header />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+
+export default {
+  name: "home",
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
