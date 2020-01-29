025262eb997377321d3270cc60318b946f0d82b3
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-29 18:42:31 +0100

    add the import/export for the component Header in views/Message.vue

--- testvuejs/src/views/Message.vue
+++ testvuejs/src/views/Message.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
 
 export default {
   name: "home",
   components: {
     HelloWorld,
+    Header
   }
 };
 </script>
