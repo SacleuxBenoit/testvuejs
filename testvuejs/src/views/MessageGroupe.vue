4fff7e99c14d1f9cfa9cee711c84fcdd95d71be8
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-29 19:11:05 +0100

    add the import/export for the component Footer in views/MessageGroupe.vue

--- testvuejs/src/views/MessageGroupe.vue
+++ testvuejs/src/views/MessageGroupe.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Header />
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "home",
   components: {
     Header,
+    Footer
   }
 };
 </script>
