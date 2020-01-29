b702c04ebb854f62e25aa78f6158939510dcdba9
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-29 18:50:47 +0100

    add the import/export for the component Header in views/About.vue

--- testvuejs/src/views/About.vue
+++ testvuejs/src/views/About.vue
@@ -1,14 +1,18 @@
 <template>
 <div>
+  <Header />
 <HelloWorld />
 </div>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
+
 export default {
   component: {
     HelloWorld,
+    Header
   }
 }
 </script>
