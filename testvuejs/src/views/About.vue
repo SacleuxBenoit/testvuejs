3725fd90af26af320e9af4bb55b0c568f03805e5
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-01-30 20:34:56 +0100

    delete the import/export of the component HelloWorld in views/About.vue

--- testvuejs/src/views/About.vue
+++ testvuejs/src/views/About.vue
@@ -1,20 +1,17 @@
 <template>
 <div>
   <Header />
-<HelloWorld />
 <Footer />
 </div>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 
 
 export default {
   component: {
-    HelloWorld,
     Header,
     Footer
   }
