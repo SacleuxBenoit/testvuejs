b1129084e0beb3c15c3b1bb0f6f4f70c319f401b
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-02 16:13:45 +0100

    add the file Home.vue with his content

---
+++ testvuejs/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="test">
+    <Header />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "test",
+  components: {
+    HelloWorld,
+    Footer,
+    Header
+  }
+};
+</script>
