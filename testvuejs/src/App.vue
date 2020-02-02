bd79335f6ac1c9a308a6c7432b30f543994e1da5
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-02 15:37:20 +0100

    delete the component HelloWorld in App.vue

--- testvuejs/src/App.vue
+++ testvuejs/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <Header/>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <Footer />
   </div>
 </template>
@@ -9,14 +8,12 @@
 <script>
 // @ is an alias to /src
 import Header from "@/components/Header.vue";
-import HelloWorld from "@/components/HelloWorld.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
   name: "home",
   components: {
     Header,
-    HelloWorld,
     Footer
   }
 };
