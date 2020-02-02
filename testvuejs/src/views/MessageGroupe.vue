0937c7aa2f28b287377980604b8888d95bb8991a
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-03 00:17:46 +0100

    change the name MessageGroupe to messageGroupe in MessageGroupe.vue

--- testvuejs/src/views/MessageGroupe.vue
+++ testvuejs/src/views/MessageGroupe.vue
@@ -11,7 +11,7 @@ import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
-  name: "MessageGroupe",
+  name: "messageGroupe",
   components: {
     Header,
     Footer
