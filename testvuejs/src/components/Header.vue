394353377e1c6bea9b66818ed538e9c6ce2d6276
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-02 17:03:10 +0100

    delete extra line for the components Header

--- testvuejs/src/components/Header.vue
+++ testvuejs/src/components/Header.vue
@@ -5,33 +5,26 @@
     dark
   >
     <v-toolbar-title>Title</v-toolbar-title>
-
-    <v-divider
+   <v-divider
       class="mx-4"
       vertical
     ></v-divider>
-
     <span class="subheading">My Home</span>
-
     <v-spacer></v-spacer>
-
     <v-toolbar-items class="hidden-sm-and-down">
       <v-btn text>
         News
       </v-btn>
-
       <v-divider vertical></v-divider>
 
       <v-btn text>
         Blog
       </v-btn>
-
       <v-divider vertical></v-divider>
 
       <v-btn text>
         Music
       </v-btn>
-
       <v-divider vertical></v-divider>
     </v-toolbar-items>
 
