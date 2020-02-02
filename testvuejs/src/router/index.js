1d3a5f8fdbba78bfead22eef2283d1b4a69a2095
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-03 00:24:01 +0100

    add the import for ContactUs in index.js

--- testvuejs/src/router/index.js
+++ testvuejs/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue"
 import About from "../views/About.vue"
+import ContactUs from "../views/ContactUs.vue"
 Vue.use(VueRouter);
 
 const routes = [
