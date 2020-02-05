1dbd8b164c3c9f571f77fb1079b89e461f29c640
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-06 00:56:57 +0100

    add content for the component Login.vue

---
+++ testvuejs/src/components/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-tab v-on="on">
+          se connecter
+        </v-tab>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline" style="color:#55b794">Se connecter</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field label="Email*" required></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field label="Password*" type="password" required></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*Champ obligatoire</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="#55b794" text @click="dialog = false">fermer</v-btn>
+          <v-btn color="#55b794" text @click="dialog = false">connexion</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false
+  })
+};
+</script>
