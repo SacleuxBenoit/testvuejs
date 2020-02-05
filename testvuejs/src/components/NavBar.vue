cd79db3937d55d83ab33a2bba88dbcd38e207e24
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-06 00:10:59 +0100

    change the color of css js and markdown in the component NavBar

--- testvuejs/src/components/NavBar.vue
+++ testvuejs/src/components/NavBar.vue
@@ -6,7 +6,7 @@
     </v-btn>
 
     <v-btn :value="activeBtn" style="color:#72dbf2" class="subtitle-1">
-      <strong style="color:#72dbf2">#CSS</strong>
+      <strong style="color:#2684FB">#CSS</strong>
     </v-btn>
 
     <v-btn :value="activeBtn" style="color:#f20574" class="subtitle-1">
@@ -14,14 +14,14 @@
     </v-btn>
 
     <v-btn :value="activeBtn" style="color:#fddb22" class="subtitle-1">
-      <strong style="color:#fddb22">#Javascript</strong>
+      <strong style="color:#FBFB26">#Javascript</strong>
     </v-btn>
 
     <v-btn :value="activeBtn" style="color:#18d95b" class="subtitle-1">
       <strong style="color:#18d95b">#VueJS</strong>
     </v-btn>
     <v-btn :value="activeBtn" style="color:#04d912" class="subtitle-1">
-      <strong style="color:#04d912">#Markdown</strong>
+      <strong style="color:#171703">#Markdown</strong>
     </v-btn>
     <v-btn :value="activeBtn" style="color:#fffAc3" class="subtitle-1">
       <strong style="color:#fffAc3">#Algorithmes</strong>
