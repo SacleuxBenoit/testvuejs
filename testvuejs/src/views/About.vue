6d2b2c650c22965029d37d60eee0a786f9fec86c
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-02-06 01:07:12 +0100

    delete the import of Header in views/About.vue

--- testvuejs/src/views/About.vue
+++ testvuejs/src/views/About.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="about">
-    <Header />
     <Footer />
      <router-view />
 
@@ -9,14 +8,12 @@
 
 <script>
 // @ is an alias to /src
-import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
   name: "about",
   components: {
     Footer,
-    Header
   }
 };
 </script>
