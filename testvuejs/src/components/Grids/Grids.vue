48c4570f4f7de17d78cf0f27ce5a7b372110a974
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-07-30 19:49:56 +0200

    add content in the component Grids.vue

--- testvuejs/src/components/Grids/Grids.vue
+++ testvuejs/src/components/Grids/Grids.vue
@@ -1,6 +1,56 @@
 <template>
     <div>
+        <v-container>
+            <v-layout>
+                <v-flex>
+                    <v-card >
+                        <v-card-text>#1</v-card-text>
+                    </v-card>
+                </v-flex>
         
+            <v-flex>
+                <v-card>
+                    <v-card-text>#1</v-card-text>
+                </v-card>
+            </v-flex>
+            </v-layout>
+
+            <v-layout>
+                <v-flex>
+                    <v-card dark tile flat>
+                        <v-card-text>#2</v-card-text>
+                    </v-card>
+                </v-flex>
+                </v-layout>
+
+            <v-layout>
+                <v-flex>
+                    <v-card>
+                        <v-card-text>#3</v-card-text>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+
+            <v-layout>
+                <v-flex>
+                    <v-card dark tile flat>
+                        <v-card-text>#4</v-card-text>
+                    </v-card>
+                </v-flex>
+
+                <v-flex>
+                    <v-card>
+                        <v-card-text>#4</v-card-text>
+                    </v-card>
+                </v-flex>
+
+                <v-flex>
+                    <v-card dark tile flat>
+                        <v-card-text>#4</v-card-text>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
     </div>
 </template>
 
@@ -9,3 +59,9 @@ export default {
     
 }
 </script>
+
+<style>
+    h1{
+        background-color: red
+    }
+</style>
