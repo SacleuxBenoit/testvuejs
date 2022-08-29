efc4bebfe4a7db17205336981e429d49ab0f72c4
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-08-29 18:56:13 +0200

    create input text for creating todos + some fake data in an array :arrow_right: TodoApp.vue

--- todo-app/src/components/TodoApp.vue
+++ todo-app/src/components/TodoApp.vue
@@ -1,6 +1,40 @@
 <template>
-  <div>
-    <h1 class="container">Hello world</h1>
+  <div class="container">
+    <h1 class="text-center">My vue Todo App</h1>
+
+    <div class="d-flex mt-5">
+      <!-- Inputs -->
+      <input type="text" placeholder="Enter text" class="form-control">
+      <button class="btn btn-warning rounded-0">Submit</button>
+    </div>
+          <!-- table -->
+      <table class="table table-bordered mt-5">
+        <thead>
+          <tr>
+            <th scope="col">Task</th>
+            <th scope="col">Status</th>
+            <th scope="col" class="text-center">#</th>
+            <th scope="col" class="text-center">#</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>first todo</td>
+            <td>Todo</td>
+            <td>
+              <div class="text-center">
+                <span class="fa fa-pen"></span>
+              </div>
+            </td>
+            <td>
+              <div class="text-center">
+                <span class="fa fa-trash"></span>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
   </div>
 </template>
 
