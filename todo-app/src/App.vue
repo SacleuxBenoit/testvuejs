99234a02d3635ee084c7c811ab27dc386936b737
SacleuxBenoit <benoitsacleux@outlook.fr>
2022-08-29 18:21:29 +0200

    :memo: delete components HelloWorld.vue && create TodoApp.vue

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TodoApp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoApp from './components/TodoApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TodoApp,
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
